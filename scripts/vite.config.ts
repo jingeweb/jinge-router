372b4c1bd24f3a9cca1635b56cb50483452f7584
yuhang <abeyuhang@gmail.com>
2024-09-13 08:49:34 +0800

    update

--- scripts/vite.config.ts
+++ scripts/vite.config.ts
@@ -1,9 +1,42 @@
+import { resolve } from 'node:path';
+import { promises as fs } from 'node:fs';
 import { defineConfig } from 'vite';
 import { jingeVitePlugin } from 'jinge-compiler';
 
-// https://vitejs.dev/config/
+const PROD = process.env.NODE_ENV === 'production';
 export default defineConfig({
-  build: {},
-
-  plugins: [jingeVitePlugin()],
+  plugins: PROD
+    ? [
+        {
+          name: 'PLUGIN',
+          load(id) {
+            return fs.readFile(id, 'utf-8').then((res) => {
+              return (
+                res
+                  // BEGIN_DROP_IN_PRODUCTION 和 END_DROP_IN_PRODUCTION 之间的代码会在构建 production 版本时删除。
+                  .replace(/\/\/ BEGIN_DROP_IN_PRODUCTION[\d\D]+?\/\/ END_DROP_IN_PRODUCTION/g, '')
+                  // Symbol() 的描述文本会在构建 production 版本时删除
+                  .replace(/\bSymbol\([^)]+\)/g, 'Symbol()')
+              );
+            });
+          },
+        },
+        jingeVitePlugin(),
+      ]
+    : [jingeVitePlugin()],
+  build: {
+    emptyOutDir: false,
+    sourcemap: true,
+    rollupOptions: {
+      external: 'jinge',
+      output: {
+        entryFileNames: `jinge-router.${PROD ? 'prod' : 'dev'}.js`,
+      },
+    },
+    minify: PROD,
+    lib: {
+      entry: resolve(__dirname, '../src/index.ts'),
+      formats: ['es'],
+    },
+  },
 });
